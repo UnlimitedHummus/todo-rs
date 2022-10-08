a28adbf0c86ea3550ffd6ff85d9a5d23c0126e90
Magnus Balzer <magnus.balzer@gmail.com>
2022-10-08 23:55:40 +0200

    Moved parsing to its own file

--- src/main.rs
+++ src/main.rs
@@ -1,73 +1,9 @@
+mod arg_parsing;
+
+use crate::arg_parsing::Args;
 use clap::Parser;
-use clap::Subcommand;
-use std::path::Path;
-use todo_rs::*;
-
-/// Simple todo lists
-#[derive(Parser, Debug)]
-#[clap(author, version, about, long_about = None)]
-struct Args {
-    /// create a new list
-    #[clap(subcommand)]
-    command: Commands,
-}
-
-#[derive(Debug, Subcommand)]
-enum Commands {
-    /// creates a new todo list
-    Create,
-    /// Add an item to the todo list
-    Add {
-        /// text of todo list item
-        #[clap(value_parser)]
-        text: String,
-    },
-    /// List all items on the list
-    List,
-    /// Mark an item on the list as done
-    Check {
-        /// Index of the item to check
-        #[clap(value_parser)]
-        item_index: usize,
-    },
-    /// Remove an item from the list
-    Remove {
-        /// Index of the item to remove
-        #[clap(value_parser)]
-        item_index: usize,
-    },
-    /// destroy the todo list
-    Destroy
-}
-
-impl Commands {
-    pub fn execute(self) { // TODO: add a Result as return value
-        match self {
-            Commands::Create => match create(Path::new(".")) {
-                Ok(_) => println!("created a new .todo file"),
-                Err(_) => println!("Warning: \".todo\" already exists. Quitting"),
-            },
-            Commands::Add { text } => {
-                add(Path::new(".todo"), &text, &mut std::io::stderr());
-            }
-            Commands::List => {
-                list(Path::new(".todo"), &mut std::io::stdout());
-            }
-            Commands::Check { item_index } => {
-                check(Path::new(".todo"), item_index, &mut std::io::stdout());
-            }
-            Commands::Remove { item_index } => {
-                remove(Path::new(".todo"), item_index, &mut std::io::stdout());
-            }
-            Commands::Destroy => {
-                destroy(Path::new(".todo"), &mut std::io::stdout());
-            }
-        }
-    }
-}
-
 
 fn main() {
     let args = Args::parse();
-    args.command.execute();
+    args.execute_command();
 }
